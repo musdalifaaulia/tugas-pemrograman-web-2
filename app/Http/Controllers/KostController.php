8df40d2255e49bcd11abfa91316e25fa3faa8043
Musdalifa Aulia Pratiwi <musdalifahaulia4@gmail.com>
2026-04-24 14:50:37 +0700

    feat: implementasi fitur read untuk kost

--- app/Http/Controllers/KostController.php
+++ app/Http/Controllers/KostController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Kost;
 
 class KostController extends Controller
 {
-    //
+    public function index()
+    {
+    return view('kosts.index', [
+    'title' => 'Data Kost',
+    'kosts' => Kost::latest()->get()
+]);
+   
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KostController;
+
+Route::get('/kosts', [KostController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
